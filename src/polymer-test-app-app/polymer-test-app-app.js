04c89eb24590b1be0893b4addf449ebddd0f1cce
Matthew Pelosi <mattpelosi@gmail.com>
2018-09-10 19:24:46 -0700

    edit functionality needs work

--- src/polymer-test-app-app/polymer-test-app-app.js
+++ src/polymer-test-app-app/polymer-test-app-app.js
@@ -1,10 +1,18 @@
 import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import "../components/get-user-button";
+import "@polymer/polymer/lib/elements/dom-repeat.js";
 
 /**
  * @customElement
  * @polymer
  */
 class PolymerTestAppApp extends PolymerElement {
+
+  ready() {
+    super.ready()
+    this.addEventListener("generate-new-user", e => this.handleClick(e));
+  }
+
   static get template() {
     return html`
       <style>
@@ -16,7 +24,7 @@ class PolymerTestAppApp extends PolymerElement {
 
       <h2>Hello [[prop1]]!</h2>
 
-      <button on-click="handleClick">Get user profile</button>
+      <get-user-button></get-user-button>
 
       <img src=[[userData.picture.thumbnail]]>
       <div class="form">
@@ -43,6 +51,18 @@ class PolymerTestAppApp extends PolymerElement {
           <input type="tel" value=[[userData.phone]] pattern="\d{10,}">
         </div>
       </div>
+      <div class="usersList">
+        <template is="dom-repeat" items="[[users]]">
+          <div class="user">
+            <img src=[[item.image]]>
+            <p>{{item.name.title}}</p>
+            <p>{{item.name.first}}</p>
+            <p>{{item.name.last}}</p>
+            <button on-click="deleteUser" data-item$={{item.username}}>Delete</button>
+            <button on-click="editUser" data-item$={{item.username}}>Edit</button>
+          </div>
+        </template>
+      </div>
     `;
   }
   static get properties() {
@@ -59,6 +79,10 @@ class PolymerTestAppApp extends PolymerElement {
       },
       title: {
         type: String
+      },
+      users: {
+        type: Array,
+        value: []
       }
     };
   }
@@ -70,10 +94,10 @@ class PolymerTestAppApp extends PolymerElement {
       }
     );
     const userData = data.results[0];
+    this.saveUserDataToLocalStorage(userData);
     this.dob = this.formatDate(userData.dob.date);
     this.title = this.setTitle(userData.name.title);
     this.userData = userData;
-    console.log(this.userData);
   }
 
   handleSelect(e) {
@@ -107,6 +131,48 @@ class PolymerTestAppApp extends PolymerElement {
       }
     }
     return title;
+  }
+
+  saveUserDataToLocalStorage(userData) {
+    const userObj = {
+      name: userData.name,
+      dob: this.formatDate(userData.dob.date),
+      address: userData.location,
+      contact: {
+        email: userData.email,
+        phone: userData.phone,
+        cell: userData.cell
+      },
+      username: userData.login.username,
+      image: userData.picture.thumbnail
+    };
+    if (localStorage.users) {
+      const users = JSON.parse(localStorage.users);
+      users.push(userObj);
+      localStorage.setItem("users", JSON.stringify(users));
+    } else {
+      localStorage.setItem("users", JSON.stringify([userObj]));
+    }
+    this.push("users", userObj);
+  }
+
+  editUser(event) {
+    const key = event.target.dataset.item;
+    for (let item in this.users) {
+      if (this.users[item].username === key) {
+        this.userData = this.users[item];
+      }
+    }
+  }
+  deleteUser(event) {
+    const key = event.target.dataset.item;
+    for (let item in this.users) {
+      if (this.users[item].username === key) {
+        const index = this.users.indexOf(this.users[item]);
+        this.splice("users", index, 1);
+      }
+    }
+    console.log(event.target.dataset);
   }
 }
 
